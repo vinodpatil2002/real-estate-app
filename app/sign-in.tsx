360f79d6611b145efb0858ce0737ed79f042107a
vinodpatil2002 <vinodnspatil@gmail.com>
2025-01-09 21:21:04 +0530

    Complete signin page UI

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,44 @@
-import {View, Text} from 'react-native'
+import {View, Text, SafeAreaView, ScrollView, Image, TouchableOpacity} from 'react-native'
 import React from 'react'
+import images from "@/constants/images";
+import icons from "@/constants/icons";
 
 const SignIn = () => {
+    const handleLogin = () => {
+
+    }
     return (
-        <View>
-            <Text>SignIn</Text>
-        </View>
+        <SafeAreaView className='bg-white h-full'>
+            <ScrollView contentContainerClassName='h-full'>
+                <Image source={images.onboarding} className='w-full h-4/6' resizeMode="contain"/>
+                <View className='px-10'>
+                    <Text className='text-base text-center uppercase font-rubik text-black-200'> Welcome to
+                        ReState </Text>
+                    <Text className='text-3xl font-rubik-bold text-black-300 text-center mt-2'>
+                        Let's get you closer to {"\n"}
+                        <Text className='text-primary-300'>
+                            Your Ideal Home
+                        </Text>
+
+                    </Text>
+                    <Text className="text-lg font-rubik text-black-200 text-center mt-12">
+                        Login To ReState with Google
+                    </Text>
+                    <TouchableOpacity className='bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5'
+                                      onPress={handleLogin}>
+                        <View className={'flex flex-row items-center justify-center'}>
+                            <Image
+                                source={icons.google}
+                                className='w-5 h-5'
+                                resizeMode={'contain'}
+                            />
+                            <Text className={'text-lg font-rubik-medium text-black-300 ml-2'}>Continue with
+                                Google</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 export default SignIn
