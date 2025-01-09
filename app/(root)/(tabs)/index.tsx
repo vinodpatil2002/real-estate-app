2d3cc50cc9c4a692386be225b848f561bdb29544
vinodpatil2002 <vinodnspatil@gmail.com>
2025-01-09 20:38:12 +0530

    Add font integration and update layout; modify app configuration and add constants for images and icons

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,21 +1,21 @@
-import { Text, View } from "react-native";
+import {Text, View} from "react-native";
 import {Link} from "expo-router";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-    <Text className='font-bold text-lg my-10'>Welcome to real-estate</Text>
-        <Link href="/sign-in">Sign In</Link>
-        <Link href="/explore">Explore</Link>
-        <Link href="/profile">Profile</Link>
-        <Link href="/properties/1">Property</Link>
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Text className='font-rubik  my-10 text-3xl'>Welcome to real-estate</Text>
+            <Link href="/sign-in">Sign In</Link>
+            <Link href="/explore">Explore</Link>
+            <Link href="/profile">Profile</Link>
+            <Link href="/properties/1">Property</Link>
 
-    </View>
-  );
+        </View>
+    );
 }
